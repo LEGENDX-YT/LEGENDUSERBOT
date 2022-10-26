01b78cac31978830feb27f14e58b31e5b588b67a
LEGENDX <116482980+ITS-LEGENDX@users.noreply.github.com>
2022-10-26 08:19:32 -0700

    Update help.py

--- Legendbot/plugins/help.py
+++ Legendbot/plugins/help.py
@@ -9,17 +9,17 @@ from ..helpers.utils import reply_id
 
 cmdprefix = Config.HANDLER
 
-menu_category = "tools"
+menu_category = "ᴛᴏᴏʟꜱ"
 
 hemojis = {
-    "admin": "👮‍♂️",
-    "bot": "🤖",
-    "fun": "🎨",
-    "misc": "🧩",
-    "tools": "🧰",
-    "utils": "🗂",
-    "extra": "➕",
-    "useless": "⚰️",
+    "ᴀᴅᴍɪɴꜱ": "🥀",
+    "ᴜꜱᴇʀʙᴏᴛ": "🥀",
+    "ꜰᴜɴ": "🥀",
+    "ᴍɪꜱᴄ": "🥀",
+    "ᴛᴏᴏʟꜱ": "🥀",
+    "ᴜᴛɪʟꜱ": "🥀",
+    "ᴇxᴛʀᴀ": "🥀",
+    "ᴜꜱᴇʟᴇꜱꜱ": "🥀",
 }
 
 
@@ -48,22 +48,22 @@ async def cmdinfo(input_str, event, plugin=False):
         if plugin:
             await eod(
                 event,
-                f"**There is no plugin or command as **`{input_str}`** in your bot.**",
+                f"🥀**ᴛʜᴇʀᴇ ɪꜱ ɴᴏ ᴘʟᴜɢɪɴ ᴏʀ ᴄᴏᴍᴍᴀɴᴅ ᴀꜱ **`{input_str}`** iɴ ʏᴏᴜʀ ʙᴏᴛ.**🥀",
             )
             return None
-        await eod(event, f"**There is no command as **`{input_str}`** in your bot.**")
+        await eod(event, f"🥀**ᴛʜᴇʀᴇ ɪꜱ ɴᴏ ᴄᴏᴍᴍᴀɴᴅ ᴀꜱ **`{input_str}`** iɴ ʏᴏᴜʀ ʙᴏᴛ.**🥀")
         return None
     except Exception as e:
         await eod(event, f"**Error**\n`{e}`")
         return None
-    outstr = f"**🕹Command :** `{cmdprefix}{input_str}`\n"
+    outstr = f"**🥀𝕮𝖔𝖒𝖒𝖆𝖓𝖉🥀 :** `{cmdprefix}{input_str}`\n"
     plugin = get_key(input_str)
     if plugin is not None:
-        outstr += f"**🔰Plugin :** `{plugin}`\n"
+        outstr += f"**🥀𝕻𝖑𝖚𝖌𝖎𝖓𝕾🥀 :** `{plugin}`\n"
         category = getkey(plugin)
         if category is not None:
-            outstr += f"**📍Category :** `{category}`\n\n"
-    outstr += f"**📜 Intro :**\n{about[0]}"
+            outstr += f"**🥀𝕮𝖆𝖙𝖊𝖌𝖔𝖗𝖞🥀 :** `{category}`\n\n"
+    outstr += f"**🥀𝕴𝖓𝖙𝖗𝖔🥀 :**\n{about[0]}"
     return outstr
 
 
@@ -79,26 +79,26 @@ async def plugininfo(input_str, event, type):
     if len(cmds) == 1 and (type is None or (type and type != "-p")):
         outstr = await cmdinfo(cmds[0], event, plugin=False)
         return outstr
-    outstr = f"**Plugin : **`{input_str}`\n"
-    outstr += f"**Commands Available :** `{len(cmds)}`\n"
+    outstr = f"**🥀𝕻𝖑𝖚𝖌𝖎𝖓𝕾 : **`{input_str}`\n"
+    outstr += f"**🥀𝕮𝖔𝖒𝖒𝖆𝖓𝖉𝖘 𝕬𝖛𝖆𝖎𝖑𝖆𝖇𝖑𝖊🥀 :** `{len(cmds)}`\n"
     category = getkey(input_str)
     if category is not None:
-        outstr += f"📍**Category :** `{category}`\n\n"
+        outstr += f"**🥀𝕮𝖆𝖙𝖊𝖌𝖔𝖗𝖞🥀 :** `{category}`\n\n"
     for cmd in sorted(cmds):
-        outstr += f"**🕹Command :** `{cmdprefix}{cmd}`\n"
+        outstr += f"**🥀𝕮𝖔𝖒𝖒𝖆𝖓𝖉🥀 :** `{cmdprefix}{cmd}`\n"
         try:
-            outstr += f"**📜Info :** __{CMD_INFO[cmd][1]}__\n\n"
+            outstr += f"**🥀𝕴𝖓𝖋𝖔🥀 :** __{CMD_INFO[cmd][1]}__\n\n"
         except IndexError:
-            outstr += "**📜Info :** `None`\n\n"
-    outstr += f"**👨‍💻 Usage : ** `{cmdprefix}help <command name>`\
-        \n**Note : **If command name is same as plugin name then use this `{cmdprefix}help -l <command name>`."
+            outstr += "**🥀𝕴𝖓𝖋𝖔🥀 :** `None`\n\n"
+    outstr += f"**🥀 𝖀𝖘𝖆𝖌𝖊 : ** `{cmdprefix}help <command name>`\
+        \n**Note : **ɪꜰ ᴄᴏᴍᴍᴀɴᴅ ɴᴀᴍᴇ ɪꜱ ꜱᴀᴍᴇ ᴀꜱ ᴘʟᴜɢɪɴ ɴᴀᴍᴇ ᴛʜᴇɴ ᴜꜱᴇ ᴛʜɪꜱ `{cmdprefix}help -l <command name>`."
     return outstr
 
 
 async def grpinfo():
-    outstr = "**Plugins in LegendBot are:**\n\n"
-    outstr += f"**👨‍💻 Usage : ** `{cmdprefix}help <plugin name>`\n\n"
-    category = ["admin", "bot", "fun", "misc", "tools", "utils", "extra", "useless"]
+    outstr = "**🥀ᴘʟᴜɢɪɴꜱ ɪɴ ʟᴇɢᴇɴᴅxʙᴏᴛ ᴀʀᴇ🥀:**\n\n"
+    outstr += f"**🥀 𝖀𝖘𝖆𝖌𝖊 : ** `{cmdprefix}help <plugin name>`\n\n"
+    category = ["ᴀᴅᴍɪɴꜱ", "ᴜꜱᴇʀʙᴏᴛ", "ꜰᴜɴ", "ᴍɪꜱᴄ", "ᴛᴏᴏʟꜱ", "ᴜᴛɪʟꜱ", "ᴇxᴛʀᴀ", "ᴜꜱᴇʟᴇꜱꜱ"]
     for legend in category:
         plugins = GRP_INFO[legend]
         outstr += f"**{hemojis[legend]} {legend.title()} **({len(plugins)})\n"
@@ -109,18 +109,18 @@ async def grpinfo():
 
 
 async def cmdlist():
-    outstr = "**Total list of Commands in your LegendBot are :**\n\n"
-    category = ["admin", "bot", "fun", "misc", "tools", "utils", "extra"]
+    outstr = "**🥀ᴛᴏᴛᴀʟ ʟɪꜱᴛ ᴏꜰ ᴄᴏᴍᴍᴀɴᴅꜱ ɪɴ ʏᴏᴜʀ ʟᴇɢᴇɴᴅxʙᴏᴛ ᴀʀᴇ🥀 :**\n\n"
+    category = ["ᴀᴅᴍɪɴꜱ", "ᴜꜱᴇʀʙᴏᴛ", "ꜰᴜɴ", "ᴍɪꜱᴄ", "ᴛᴏᴏʟꜱ", "ᴜᴛɪʟꜱ", "ᴇxᴛʀᴀ"]
     for legend in category:
         plugins = GRP_INFO[legend]
         outstr += f"**{hemojis[legend]} {legend.title()} ** - {len(plugins)}\n\n"
         for plugin in plugins:
             cmds = PLG_INFO[plugin]
-            outstr += f"• **{plugin.title()} has {len(cmds)} commands**\n"
+            outstr += f"• **{plugin.title()} ʜᴀꜱ {len(cmds)} ᴄᴏᴍᴍᴀɴᴅꜱ**\n"
             for cmd in sorted(cmds):
                 outstr += f"  - `{cmdprefix}{cmd}`\n"
             outstr += "\n"
-    outstr += f"**👨‍💻 Usage : ** `{cmdprefix}help -l <command name>`"
+    outstr += f"**🥀 𝖀𝖘𝖆𝖌𝖊 : ** `{cmdprefix}help -l <command name>`"
     return outstr
 
 
@@ -190,10 +190,10 @@ async def _(event):
             return await eod(event, "__Invalid plugin name recheck it.__")
         except Exception as e:
             return await eod(event, f"**Error**\n`{e}`")
-        outstr = f"**📜 {input_str.title()} has {len(cmds)} commands**\n"
+        outstr = f"**🥀 {input_str.title()} has {len(cmds)} commands**\n"
         for cmd in cmds:
             outstr += f"  - `{cmdprefix}{cmd}`\n"
-        outstr += f"**👨‍💻  Usage : ** `{cmdprefix}help -l <command name>`"
+        outstr += f"**🥀  𝖀𝖘𝖆𝖌𝖊 : ** `{cmdprefix}help -l <command name>`"
     await eor(event, outstr, aslink=True, linktext="Total Commands of LegendBot are :")
 
 
@@ -209,7 +209,7 @@ async def _(event):
 async def _(event):
     "To get dc of your bot"
     result = await event.client(functions.help.GetNearestDcRequest())
-    result = f"**📜Dc details of your account:**\
+    result = f"**🥀Dc details of your account:**\
               \n**Country :** {result.country}\
               \n**Current Dc :** {result.this_dc}\
               \n**Nearest Dc :** {result.nearest_dc}\
