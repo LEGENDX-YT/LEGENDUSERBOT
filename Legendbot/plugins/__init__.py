6b93977362ca4ab46593424ce6f1d8238c21bdba
LEGENDX <116482980+ITS-LEGENDX@users.noreply.github.com>
2022-10-26 07:53:34 -0700

    Update __init__.py

--- Legendbot/plugins/__init__.py
+++ Legendbot/plugins/__init__.py
@@ -60,17 +60,17 @@ ban_pic = "./Legendbot/helpers/resources/pics/ban.jpg"
 
 
 # channel
-my_channel = Config.YOUR_CHANNEL or "LegendBot_OP"
-my_group = Config.YOUR_GROUP or "LegendBot_AI"
+my_channel = Config.YOUR_CHANNEL or "Y_RUS_NETWORK"
+my_group = Config.YOUR_GROUP or "Y_RUS_SUPPORT"
 if "@" in my_channel:
     my_channel = my_channel.replace("@", "")
 if "@" in my_group:
     my_group = my_group.replace("@", "")
 
 # My Channel
-chnl_link = "https://t.me/LegendBot_AI"
+chnl_link = "https://t.me/Y_RUS_NETWORK"
 Legend_channel = f"[Lêɠêɳ̃dẞø† ]({chnl_link})"
-grp_link = "https://t.me/LegendBot_OP"
+grp_link = "https://t.me/Y_RUS_SUPPORT"
 Legend_grp = f"[Lêɠêɳ̃dẞø† ]({grp_link})"
 
 
