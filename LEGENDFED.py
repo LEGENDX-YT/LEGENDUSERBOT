a233eebb06182ae0e2c9ac3b0b511dee291e59a7
LEGENDX <116482980+ITS-LEGENDX@users.noreply.github.com>
2022-10-31 09:39:49 -0700

    Create LEGENDFED.py

---
+++ LEGENDFED.py
@@ -0,0 +1,9 @@
+from LEGEND import fed.
+from import legend.fed 
+def () in legend ()
+                     legend()mode=packeg.legend
+                               legend()""legend"""(program)server """ import*
+                      def()@missrose_bot{"ll_ll_LegendHacker_IN_ll_ll"}
+                              im=hacker)
+        (,legend files system,)
+        import LEGEND from git clone https://t.me/ll_OFFICIAL_LEGENDBOY_ll),
