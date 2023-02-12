b8dbdbfd86cfadc5c15aca4ffed8df738d4e6036
【★ʟɛɢɛռɖ ✘ Hคckeℝ  】 <104726886+LEGENDX-YT@users.noreply.github.com>
2023-02-12 08:29:42 -0800

    Create From.py

---
+++ From.py
@@ -0,0 +1,15 @@
+import(_).userbot
+import(my).musicbot
+import(legend).management.txt
+import(-Os.Eval:Var{On/Off}.patter(*op)
+       import(javaScrips). password.zip(extrect.files)[buttonsurl:||OS||:]
+       pass
+         class
+           def 
+             in
+              None
+               False
+                 True
+                   if
+                    type("restart reboot /start user this commands")
+       pass()
